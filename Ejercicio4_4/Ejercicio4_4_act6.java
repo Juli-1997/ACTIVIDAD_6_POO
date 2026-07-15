0a7b4530a2e933947ba317f14ee9c13d25fc996b
Juli-1997 <jcastellarp@unal.edu.co>
2026-07-15 16:45:52 -0500

    Add files via upload

---
+++ Ejercicio4_4/Ejercicio4_4_act6.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mypoo2.ejercicio4_4_act6;
+
+/**
+ *
+ * @author HP-255-G10
+ */
+public class Ejercicio4_4_act6 {
+
+    public static void main(String[] args) {
+
+        profesor profesor1 = new profesor_titular();
+
+        profesor1.imprimir();
+
+    }
+
+}
---
+++ Ejercicio4_4/profesor_titular.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mypoo2.ejercicio4_4_act6;
+
+/**
+ *
+ * @author HP-255-G10
+ */
+public class profesor_titular extends profesor {
+
+    // Sobrescribe el método de la clase padre
+    @Override
+    protected void imprimir() {
+        System.out.println("Es un profesor titular.");
+    }
+
+}
